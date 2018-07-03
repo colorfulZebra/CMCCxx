230c658f65fe4b60a0a9259f6c4194bcaf7e2f93
colorfulZebra <colorful_zebra@139.com>
2018-07-03 10:00:08 +0800

    use element-ui

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="mainContainer">
+    <h1>{{ hello }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContainer',
+  data () {
+    return {
+      hello: 'Hello world'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MainContainer from '@/components/MainContainer'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'MainContainer',
+      component: MainContainer
+    },
+    {
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
